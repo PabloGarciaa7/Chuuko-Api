21d905604e9dd62fdcdd6d7108be8c47dbf830ac
PabloGarciaa7 <93121647+PabloGarciaa7@users.noreply.github.com>
2023-05-03 09:08:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.use('/api', apiRoutes);
 app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express funcionando");
 });
